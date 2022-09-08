7b8cd2cb2c48e1e8ba974285030e0dc049236bc5
vvalugonzalez <valu322@gmail.com>
2022-09-07 23:00:28 -0300

    DOM

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ let total= 0;
 const servicios = [];
 const carrito = [];
 
+
 class Servicio {
     constructor( nombre, precio) {
         
@@ -28,40 +29,43 @@ function generarServicios() {
 generarServicios()
 
 //ciclo principal de la simulacion que se ejecuta mientras servicio sea true
-while(servicio){
-    compra = elegirServicio();
-    let cantidad = parseFloat(prompt("Ingrese cantidad de horas de servicio, en caso de ser Souvenir cantidad que se requieran: "))
-    total = totalCompra(compra, cantidad);
-    servicio = confirm ("¿Desea agregar otro servicio?")
-}
+ while(servicio){
+     compra = elegirServicio();
+     let cantidad = parseFloat(prompt("Ingrese cantidad de horas de servicio, en caso de ser Souvenir cantidad que se requieran: "))
+     total = totalCompra(compra, cantidad);
+     servicio = confirm ("¿Desea agregar otro servicio?")
+ }
+
 
 alert("Se compraron " + carrito.length + " " + "servicios."+ "\n El precio final es de: " + total);
 listarServiciosComprados();
 
 
+
+
 //funcion para elegir el servicio con validacion
-function elegirServicio(){
-    let compra = parseFloat(prompt("Ingrese Servicio que desea comprar (1,2 o 3): \n 1-Servicio SelfieTag \n 2-Servicio de Cabina de Fotos \n 3-Servicio de Souvenir  "))
-    if (compra == 1){
-        carro("Selfie-Tag")
-        return compra;
-        
-    }
-    else if(compra == 2){
-        carro("Cabina de Fotos")
-        return compra;
-    }
-    else if (compra == 3 ){
-        carro("Souvenir")
-        return compra;
-    }
-    else{
-        console.log("El numero ingresado no es correcto")
-        elegirServicio();
-        
-    }
-    
-}
+ function elegirServicio(){
+     let compra = parseFloat(prompt("Ingrese Servicio que desea comprar (1,2 o 3): \n 1-Servicio SelfieTag \n 2-Servicio de Cabina de Fotos \n 3-Servicio de Souvenir  "))
+     if (compra == 1){
+         carro("Selfie-Tag")
+         return compra;
+      
+     }
+     else if(compra == 2){
+         carro("Cabina de Fotos")
+         return compra;
+     }
+     else if (compra == 3 ){
+         carro("Souvenir")
+         return compra;
+     }
+     else{
+         console.log("El numero ingresado no es correcto")
+         elegirServicio();
+      
+     }
+  
+ }
     
 //funcion para calcular el monto total de los servicios comprados
 function totalCompra(compra, cantidad){
@@ -95,7 +99,6 @@ function listarServiciosComprados() {
 }
 
 function buscarServicio() {
-    debugger
     let nombreServ = prompt("Ingresa el nombre del servicio a buscar:").toUpperCase()
     let resultado = servicios.find((servicio)=> servicio.nombre.includes(nombreServ))
         if (resultado !== undefined) {
@@ -104,3 +107,4 @@ function buscarServicio() {
             console.warn("No se encontró un elemento coincidente.")
         }
 }
+
