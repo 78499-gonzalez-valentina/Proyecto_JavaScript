88f841193390685097cc319e13d1af6052fc59ad
vvalugonzalez <valu322@gmail.com>
2022-09-13 21:59:55 -0300

    prueba

--- js/dom.js
+++ js/dom.js
@@ -1,12 +1,35 @@
- const tabla = document.getElementById("tabla")
- const filtrarServicio = document.getElementById("filtrarServicio")
+const tabla = document.getElementById("tabla")
+const inputFiltrar = document.getElementById("inputFiltrar")
+const servicios = [];
+const carrito = [];
 
- function cargarServicios(){
+
+class Servicio {
+    constructor(id, nombre, precio) {
+        this.id = id
+        this.nombre = nombre
+        this.precio = precio
+        
+    }
+}
+
+function generarServicios() {
+    servicios.push(new Servicio( 545,"SELFIETAG", 2000))
+    servicios.push(new Servicio(412, "CABINA DE FOTOS", 5000))
+    servicios.push(new Servicio(874, "SOUVENIR", 300))
+ 
+}
+
+generarServicios()
+ 
+ function cargarServicios(array){
  let fila = ""
-     servicios.forEach(servicio => {
+     array.forEach(servicio => {
          fila = `<tr>
+                     <td>${servicio.id}</td>
                      <td>${servicio.nombre}</td>
                      <td>${servicio.precio}</td>
+                     <td><button id="btn${servicio.numero}">+</button></td>
                  </tr>`   
                  tabla.innerHTML += fila
            
@@ -14,6 +37,9 @@
 
 
  }
+
+ cargarServicios(servicios)
+
  function crearElementoHTML(){
      const parrafo = document.createElement("p")
          parrafo.id = "parrafo"
@@ -23,6 +49,25 @@
  }
 
 
-cargarServicios()
 
 
+
+function filtrarServicios() { //FILTRAR PRODUCTOS EN LA TABLA INGRESANDO PARTE DEL NOMBRE
+    debugger
+    inputFiltrar.value = inputFiltrar.value.trim().toUpperCase()
+    if (inputFiltrar.value !== "") {
+        const resultado = servicios.filter(servicio => servicio.nombre.includes(inputFiltrar.value))
+              if (resultado.length === 0) {
+                console.clear()
+                console.warn("No se encontraron productos.")
+                cargarServicios(servicios)
+              } else {
+                cargarServicios(resultado)
+              }
+    }
+    else {
+        cargarServicios(servicios)
+    }
+}
+
+inputFiltrar.addEventListener("input", filtrarServicios) //A medida que escribimos.
--- js/eventos.js
+++ js/eventos.js
@@ -1,7 +1,9 @@
 const agregar = document.querySelector("#agregar")
 
 function cargar(){
-    alert("Servicio agregado ")
+    debugger
+    servicios.push(new Servicio(id.value, nombre.value, precio.value))
+    cargarServicios(servicios)
 }
 
 
@@ -14,6 +16,10 @@ inputs.forEach(input=> {
     input.addEventListener("blur",()=> input.className = "")
 
 })
+
+const id = document.querySelector("#inputID")
+const nombre = document.querySelector("#inputNombre")
+const precio = document.querySelector("#inputPrecio")
 
 
 
