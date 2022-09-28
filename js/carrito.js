d96f59a65e553ddd021f49351aae3fb0d30070d3
vvalugonzalez <valu322@gmail.com>
2022-09-28 11:39:54 -0300

    carrito

--- js/carrito.js
+++ js/carrito.js
@@ -1,31 +1,33 @@
 const limpiar = document.getElementById("limpiar")
 const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
+
+
+let carritoHtml = (serv) =>{
+    return `<tr>
+                <td>${serv.nombre}</td>
+                <td>$ ${serv.precio * serv.quantity}</td>
+                <td> X${serv.quantity}</td>
+                <td><button class= "borrar" id="btn${serv.id}">-</button></td>
+            </tr>`
+} 
+
 function recuperoCarrito(carrito) {
     
     carrito = JSON.parse(localStorage.getItem("carrito"))
     let tabla = document.querySelector("tbody")
     let total = 0
-        carrito.forEach(serv => {
-            let fila = `<tr>
-                            <td>${serv.nombre}</td>
-                            <td>$ ${serv.precio * serv.quantity}</td>
-                            <td> X${serv.quantity}</td>
-                            <td><button class= "borrar" id="btn${serv.id}">-</button></td>
-                        </tr>`
-
-                        tabla.innerHTML += fila
-                      
-                        
-         });  
-
+    tabla.innerHTML = carrito.map(serv => carritoHtml(serv))
+                                          
+        
 }
+
 
 recuperoCarrito(carrito)
 
 function eliminarServicio(array, id)
  {
-    debugger
+    
    //Buscar el indice del objeto con el id en el array
     const objIndex = array.findIndex((obj) => obj.id == id);
     array.splice(objIndex, 1);
@@ -36,23 +38,23 @@ function eliminarServicio(array, id)
 
  function eliminarServicioDelCarrito(id)
  {
-    debugger
     innerHTML = ""
      let newCarrito = eliminarServicio(carrito, id)
      localStorage.setItem("carrito", JSON.stringify(newCarrito))
      recuperoCarrito(newCarrito)
+     
  }
 
 
 
  function eliminarEnBotones()
  {  
+    debugger
      carrito.forEach(serv => {
         const borrar = document.querySelector(`#btn${serv.id}`)
         borrar.addEventListener("click",  ()=> eliminarServicioDelCarrito(`${serv.id}`))
      })
      
  }
-
-eliminarEnBotones()
+ eliminarEnBotones()
 
